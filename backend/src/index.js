3569d564969eda2f7a1328d510f3876c885d917c
LeTarzan <leandrorevolve@gmail.com>
2020-10-31 23:07:08 -0300

    Add GetFileById, DeleteFileById and config middleware static of express

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose')
+const path = require('path')
 require('dotenv').config()
 
 const app = express()
@@ -11,11 +12,12 @@ mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGOD
   useUnifiedTopology: true
 })
 
-
-
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
+app.use('/files',
+  express.static(path.resolve(__dirname, '../', 'tmp', 'uploads'))
+)
 
 app.use(require('./routes'))
 
